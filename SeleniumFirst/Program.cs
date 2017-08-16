e9243b941b1df91d02c6b1b7cd5e9ce06c0a6648
nDudarenkov <ndudarenkov@gmail.com>
2017-08-16 10:33:29 +0300

    init

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+
+        //create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
+        static void Main(string[] args)
+        {
+            
+        }
+        [SetUp]
+        public void Initialize ()
+        {
+            //navigate to Google page
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+            Console.WriteLine("Opened URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Enter text(element, value, type)
+
+
+            //Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+            //Perform Ops
+            element.SendKeys("executeautomation");
+            Console.WriteLine("Execute Test");
+        }
+
+        [Test]
+        public void NextTest()
+        {
+            //Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+            //Perform Ops
+            element.SendKeys("executeautomation");
+            Console.WriteLine("Execute Test");
+
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Close the browser");
+        }
+    }
+
+}
